c2e0ad86c2f45e049574e2aad85caabdfabb0785
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-05 16:04:29 +0700

    add case 2

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -78,7 +78,16 @@ namespace Exercise4
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
+            }
         }
     }
 }
